db6a09bb9ceb23ebd2d011c94f2c65a3a8b61a77
J.D. Bean <jonathandbean@gmail.com>
2018-02-13 00:43:31 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,9 @@
 def speak_to_grandma(phrase)
   if phrase == 'I LOVE YOU GRANDMA!'
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.uppercase
+  else phrase == phrase.uppercase
     puts "NO, NOT SINCE 1938!"
   else
-    return "HUH?! SPEAK UP, SONNY!"
+    puts "HUH?! SPEAK UP, SONNY!"
   end
 end
