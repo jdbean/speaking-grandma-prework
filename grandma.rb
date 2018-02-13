3ed6a12f72350d6428a7fefa31348b78430bdc52
J.D. Bean <jonathandbean@gmail.com>
2018-02-13 00:42:44 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -18,6 +18,6 @@ def speak_to_grandma(phrase)
   elsif phrase == phrase.uppercase
     puts "NO, NOT SINCE 1938!"
   else
-    puts "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
